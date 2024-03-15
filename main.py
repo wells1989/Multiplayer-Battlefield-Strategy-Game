aa08b524b7a4dd804eb005fc19e6dac0f486d315
Paul Wells <wellsy95@hotmail.com>
2024-03-15 12:20:44 +0000

    bug fixed and functional game

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from globals.classes import HQ, Terrain
 from utils.game_setup import start_game, generate_game_variables, reset_game
 from utils.game_logic import is_touching, ability_checker, take_turn, battle, last_survivor, update_scores, check_sound, update_info_message, battle_lost, battle_won, mute_event_handler
 from utils.game_display import pygame_setup, game_choices, display_rules_popup
-from utils.event_handlers import handle_key_event, handle_quit_event
+from utils.event_handlers import handle_key_event, handle_quit_event, buttons_event_handler, handle_cell_clicks
 
 ## Main game loop
 
@@ -27,72 +27,42 @@ while running:
             row = event.pos[1] // cell_size 
             col = event.pos[0] // cell_size
 
-            # Restart game button
-            restart_button = pygame.Rect(grid_size * cell_size + 5, grid_size * cell_size + 10, cell_size - 10, 40)
-            if restart_button.collidepoint(event.pos):
+            # on screen buttons and inactive state
+            button = buttons_event_handler(game_active, event, mute_event_handler, grid_size, cell_size, start_game, players, grid_window, screen_size, display_rules_popup, reset_game, game_choices, sound_active, info_message, grid)
+            if button == "restart":
                 grid, screen, cols, rows, grid_size, running, current_player, clicked_cell, info_message, game_active, cell_size = start_game(players, grid_window, screen_size, grid_size)
                 continue
-            
-            # View Rules button
-            rules_button = pygame.Rect(cell_size * grid_size + cell_size + 5, grid_size * cell_size + 10, cell_size-10, 40)
-            if rules_button.collidepoint(event.pos):
-                # reset game button, returns True from display_rules_popup with new player count
-                if display_rules_popup(screen_size):
-                    player_count, grid_window, screen_size, grid_size, water_cell_color, desert_cell_color, mountains_cell_color, current_cell_color, player1, player2, player3, player4, players, selected_attribute,attribute_values, sound_active, grid, screen, rows, cols, running, current_player, clicked_cell, info_message, game_active, cell_size = reset_game(game_choices)
-                    continue
-
-            # mute / unmute button
-            mute_button = pygame.Rect((cell_size * (grid_size - 1) + 60, grid_size * cell_size + 5, 40, 40))
-            if mute_button.collidepoint(event.pos):
+            elif button == "reset":
+                player_count, grid_window, screen_size, grid_size, water_cell_color, desert_cell_color, mountains_cell_color, current_cell_color, player1, player2, player3, player4, players, selected_attribute,attribute_values, sound_active, grid, screen, rows, cols, running, current_player, clicked_cell, info_message, game_active, cell_size = reset_game(game_choices)
+                continue
+            elif button == "mute":
                 sound_active, info_message = mute_event_handler(sound_active, info_message)
                 continue
-
-            # if game has been won ...
-            if not game_active:
+            elif button == "inactive_game":
                 info_message = "Game not active, please start new game"
                 continue
-
-            # info / error message control
+            
+            # cell click functionality
             info_message = ""
             if 0 <= row < grid_size and 0 <= col < grid_size:
-                cell = grid[row][col]
-            else:
-                info_message = update_info_message(info_message, "Clicked outside grid", sound_active, sounds)
-                continue
+                cell = grid[row][col] 
 
-            if not is_touching(row, col, current_player, grid, grid_size) and cell.controlled_by != current_player.name:
-                info_message = update_info_message(info_message, "Need to click on touching cells", sound_active, sounds)
-                continue
-            elif isinstance(cell, Terrain) and cell.controlled_by == "":
-                if cell.type == "Water" and not ability_checker(row, col, current_player, "naval", grid, grid_size):
-                    info_message = update_info_message(info_message, "Need a navy to access water", sound_active, sounds)
+            cell_click_result = handle_cell_clicks(row, col, grid, grid_size, cell_size, update_info_message, sound_active, sounds, is_touching, current_player, ability_checker, info_message, players, rows, cols, battle, check_sound, battle_won, battle_lost, update_scores, take_turn)
+            if cell_click_result:
+                if cell_click_result[0] == "error":
+                    info_message = update_info_message(info_message, cell_click_result[1], sound_active, sounds)
                     continue
-                elif cell.type == "Mountains" and not ability_checker(row, col, current_player, "spec_ops", grid, grid_size):
-                    info_message = update_info_message(info_message, "Need spec_ops to access mountains", sound_active, sounds)
+                elif cell_click_result[0] == "attacker won":
+                    cell = cell_click_result[1]
+                    defending_player = cell_click_result[2]
+                    game_active, info_message, clicked_cell, current_player = battle_won(cell, players, defending_player, clicked_cell, current_player, check_sound, sounds, sound_active, game_active, update_scores, take_turn, info_message, grid, rows, cols, row, col)
                     continue
-            elif cell.controlled_by and cell.controlled_by != current_player.name:
-                if isinstance(cell, Terrain) and cell.type == "Water" and not ability_checker(row, col, current_player, "naval", grid, grid_size):
-                    info_message = update_info_message(info_message, "Need a navy to Attack water", sound_active, sounds)
+                elif cell_click_result == "stalemate":
+                    info_message, clicked_cell, current_player = battle_lost("stalemate, changing player", clicked_cell, current_player, grid, rows, cols, players, update_scores, take_turn)
                     continue
-                elif isinstance(cell, Terrain) and cell.type == "Mountains" and not ability_checker(row, col, current_player, "spec_ops", grid, grid_size):
-                    info_message = update_info_message(info_message, "Need spec_ops to Attack mountains", sound_active, sounds)
+                elif cell_click_result == "attack failed":
+                    info_message, clicked_cell, current_player = battle_lost("attack failed, changing player", clicked_cell, current_player, grid, rows, cols, players, update_scores, take_turn) 
                     continue
-
-                # Battle logic
-                else:
-                    check_sound(sounds, "battle", sound_active)
-                    defending_player = next((player for player in players if player.name == cell.controlled_by), None)
-                    if battle(grid, row, col, current_player, players, rows, cols, grid_size, defending_player) == "attacker won":
-                        game_active, info_message, clicked_cell, current_player = battle_won(cell, players, defending_player, clicked_cell, current_player, check_sound, sounds, sound_active, game_active, update_scores, take_turn, info_message, grid, rows, cols, row, col)
-                        continue
-                    elif battle(grid, row, col, current_player, players, rows, cols, grid_size, defending_player) == "stalemate, new round":
-                        info_message, clicked_cell, current_player = battle_lost("stalemate, changing player", clicked_cell, current_player, grid, rows, cols, players, update_scores, take_turn)
-                        continue
-                    else:
-                        info_message, clicked_cell, current_player = battle_lost("attack failed, changing player", clicked_cell, current_player, grid, rows, cols, players, update_scores, take_turn) 
-                        continue
-
-            # normal cell clicking functionality    
             else:
                 clicked_cell = grid[row][col]
                 check_sound(sounds, "pop", sound_active)
@@ -106,7 +76,7 @@ while running:
             cell.controlled_by = current_player.name
 
             current_player, clicked_cell = take_turn(players, current_player, update_scores, grid, rows, cols, clicked_cell)
-        
+            
 
         # key events (on already controlled cells)
         elif event.type == KEYDOWN:
@@ -120,4 +90,3 @@ while running:
 # Quit Pygame
 pygame.quit()
 sys.exit()
-
